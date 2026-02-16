e5bce72363e9390d54ec89f82e27552bcd363589
francistudor <uuidpro@proton.me>
2026-02-16 17:26:04 +0100

    Removed Unnecessary Import

--- src/main/java/frc/robot/subsystems/intake/IntakeDrop.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeDrop.java
@@ -8,7 +8,6 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.robot.Constants.IntakeDropConstants;
-import frc.robot.commands.modules.intake.IntakeDropCommand;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
