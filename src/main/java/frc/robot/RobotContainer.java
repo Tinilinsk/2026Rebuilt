5889770f4e1e3399e85b41b2b5d9862b24dee2ae
Tinilinsk <tema.ryabkov@gmail.com>
2026-03-08 15:25:04 +0100

    Deleted old code for auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -118,10 +118,6 @@ public class RobotContainer {
         public SwerveSubsystem getSwerve() {
     return drivebase;
 }
-
-        public SwerveSubsystem getSwerve() {
-    return drivebase;
-}
         /**
          * Use this method to define your button->command mappings. Buttons can be
          * created by
@@ -243,36 +239,6 @@ public class RobotContainer {
                         DriverStation.reportWarning("Skipping invalid auto '" + autoName + "'", ex.getStackTrace());
                         return null;
                 }
-                // try{
-                //         // Load the path you want to follow using its name in the GUI
-                //         // Create a list of waypoints from poses. Each pose represents one waypoint.
-                //         // The rotation component of the pose should be the direction of travel. Do not use holonomic rotation.
-                //         List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
-                //                 new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0)),
-                //                 new Pose2d(1.0, 0.0, Rotation2d.fromDegrees(0)),
-                //                 new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0))
-                //         );
-
-                //         PathConstraints constraints = new PathConstraints(1.0, 0.5, 2 * Math.PI, 4 * Math.PI); // The constraints for this path.
-                //         // PathConstraints constraints = PathConstraints.unlimitedConstraints(12.0); // You can also use unlimited constraints, only limited by motor torque and nominal battery voltage
-
-                //         // Create the path using the waypoints created above
-                //         PathPlannerPath path = new PathPlannerPath(
-                //                 waypoints,
-                //                 constraints,
-                //                 null, // The ideal starting state, this is only relevant for pre-planned paths, so can be null for on-the-fly paths.
-                //                 new GoalEndState(0.0, Rotation2d.fromDegrees(0)) // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
-                //         );
-
-                //         // Prevent the path from being flipped if the coordinates are already correct
-                //         path.preventFlipping = true;
-
-                //         // Create a path following command using AutoBuilder. This will also trigger event markers.
-                //         return AutoBuilder.followPath(path);
-                // } catch (Exception e) {
-                //         DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
-                //         return Commands.none();
-                // }
         }
 
         private String getDashboardSelectedAutoName() {
